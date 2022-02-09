26d036ec51e3f06beeab393a7648d449cbe9857b
Odumz <odumz20@gmail.com>
2022-02-09 22:57:57 +0100

    welcome card added

---
+++ components/WelcomeCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex w-full justify-between mt-12 mb-8 items-baseline">
+    <div class="">
+      <h1 class="text-3xl text-muted font-semibold">Welcome back, Olivia</h1>
+      <p class="text-muted text-sm">Track, manage and forecast your customers and orders.</p>
+    </div>
+    <div class="flex items-center gap-4 justify-around">
+      <button class="flex items-center border rounded border-primary justify-center py-2 px-3">
+        <img src="../assets/img/import.svg" alt="import-icon">
+        <span class="px-3">Import</span>
+      </button>
+      <button class="flex items-center justify-center py-2 px-3 bg-primary text-white rounded">
+        <img src="../assets/img/plus.svg" alt="plus-icon">
+        <span class="px-3">Add new</span>
+      </button>
+    </div>
+
+  </div>
+</template>
